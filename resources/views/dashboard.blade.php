90e324e7dcc11968919dbfb6e04354139f0c1d65
joeDastan <yusef.dastan@yahoo.com>
2025-11-19 13:39:44 +0330

    databaseSeed-dashboardView

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        @if(auth()->user()->posts()->count()===0)
+        No Post Yet
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
@@ -11,8 +13,18 @@
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        @else
+        Your Posts :
+        <div class="grid auto-rows-min gap-2 md:grid-cols-3">
+                @foreach (auth()->user()->posts as $post)
+                    <div class="bg-gray-800 text-white rounded p-5">
+                        <h3 class="text-xl font-bold">{{ $post->title }}</h3>
+                        <h6 class="text-xs">{{ $post->user->email }}</h6>
+                        </br>
+                        <p>{{ $post->content }}</p>
+                    </div>
+                @endforeach
         </div>
+        @endif
     </div>
 </x-layouts.app>
